a008d6a3770cdcc00b8893a2242004523d58ffbb
eminishimoto <eminishimoto5@gmail.com>
2023-03-24 13:27:55 +0000

    ionic app

---
+++ ionic_week4_simpleapp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_week4_simpleapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
