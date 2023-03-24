f1826995494c270fec1fd4ca0ea1e69b5eb58a62
eminishimoto <eminishimoto5@gmail.com>
2023-03-24 14:59:55 +0000

    add constructor

--- ionic_week4_simpleapp/src/app/home/home.page.ts
+++ ionic_week4_simpleapp/src/app/home/home.page.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  myVariable:string = 'The force is with me';
+  updateMyValue(){
+    this.myVariable = 'Now the force may be with you'
+  }
 
 }
