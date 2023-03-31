ef59bf34e77600eff89756d0ea0db78f31b49e03
eminishimoto <eminishimoto5@gmail.com>
2023-03-31 17:32:28 +0000

    add bottons

--- rsimpleapp/src/app/home/home.page.ts
+++ rsimpleapp/src/app/home/home.page.ts
@@ -10,4 +10,12 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage {
   constructor() {}
+    myVariable:string = "The force with me!"; //add write variable 
+    myVariable2:string = "Hello from cct!"; //add write variable 
+    updateMyValue(){ //add
+      this.myVariable = "Now the force is even stronger!";//add
+    }
+      updateMyValue2(){ //add
+        this.myVariable2 = "Hello from Ionic mobile development!";//add
+      } 
 }
